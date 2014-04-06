d1557f89dda71a67a8502046b472a03974424db4
simonboeuf <simon.boeuf@gmail.com>
2014-04-06 14:31:39 +0200

    Fixed auto increment on carts

--- includes/cart_data.php
+++ includes/cart_data.php
@@ -3,7 +3,7 @@ include_once("data.php");
 
 function create_cart($user, $product)
 {
-	$table = 'carts';
+	$table = 'cart';
 	$fields['cart_user'] = $user;
 	$fields['cart_product'] = $product;
 	return (insert($table, $fields));
@@ -11,7 +11,7 @@ function create_cart($user, $product)
 
 function update_cart($id, $user, $product)
 {
-	$table = 'carts';
+	$table = 'cart';
 	$fields['cart_user'] = $user;
 	$fields['cart_product'] = $product;
 	$where['cart_id'] = $id;
@@ -20,14 +20,14 @@ function update_cart($id, $user, $product)
 
 function delete_cart($id)
 {
-	$table = 'carts';
+	$table = 'cart';
 	$where['cart_id'] = $id;
 	return (delete($table, $where));
 }
 
 function get_carts()
 {
-	$table = 'carts';
+	$table = 'cart';
 	$where = NULL;
 	$res = get($table, $where);
 	return (ret_data(get($table, $where)));
@@ -35,28 +35,28 @@ function get_carts()
 
 function get_carts_by_id($id, $order = NULL)
 {
-	$table = 'carts';
+	$table = 'cart';
 	$where['cart_id'] = $id;
 	return (ret_data(get($table, $where)));
 }
 
 function get_carts_by_user($user)
 {
-	$table = 'carts';
+	$table = 'cart';
 	$where['cart_user'] = $user;
 	return (ret_data(get($table, $where)));
 }
 
 function get_carts_by_product($product)
 {
-	$table = 'carts';
+	$table = 'cart';
 	$where['cart_product'] = $product;
 	return (ret_data(get($table, $where)));
 }
 
 function is_product_in_cart($user, $product)
 {
-	$table = 'carts';
+	$table = 'cart';
 	$where['cart_user'] = $user;
 	$where['cart_product'] = $product;
 	if (mysqli_fetch_assoc(get_count($table, $where))['count'] == 0)
